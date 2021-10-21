d3f00ddc27eb116f60fe131891012f4ee603f8ff
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-10-21 16:15:31 -0500

    completed first java exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,34 @@
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//    }
+//}
+
+//THIS ONE BELOW WILL PRINT OUT ON TWO DIFFERENT LINES
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        System.out.println("Hello, ");
+//        System.out.println("World!");
+//    }
+//}
+
+//THIS WILL PRINT OUT HELLO WORLD MESSAGE ON SAME LINE
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello ");
+        System.out.print("World!");
+    }
+}
+
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        System.out.print("Hello World!");
+//    }
+//}
+
+//class HelloWorld {
+//    public static void main(String[] args) {
+//        System.out.print("Hello World!");
+//    }
+//}
+
