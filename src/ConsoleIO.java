5c8cca1907e4ef364d6de0ddb0bf75d5b12074fb
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-10-22 14:28:55 -0500

    created consoleExercise java file for class exercise

--- src/ConsoleIO.java
+++ src/ConsoleIO.java
@@ -6,19 +6,22 @@ public class ConsoleIO {
         Scanner scanner = new Scanner(System.in);
 
         //TODO LECTURE STUFF FOR 10/22
-//        String name = "Yo";
-//        System.out.printf("Hello, %s. Nice to see you.\n", name);
-//
-//        String greet = "YO";
-//        String nombre = "MATT";
-//        System.out.printf("%s, %s", nombre, greet);
+        String name = "Yo";
+        System.out.printf("Hello, %s. Nice to see you.\n", name);
+
+        String greet = "YO";
+        String nombre = "MATT";
+        System.out.printf("%s, %s", nombre, greet);
 
         System.out.print("Enter something: ");
 
 //        String userInput = scanner.next(); Commented out to do next line
         String userInput = scanner.nextLine();
 
-        System.out.println("You entered: -->\"" + userInput + "\" <--");
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+        System.out.printf("You entered %s", userInput);
+
 
 
     }
