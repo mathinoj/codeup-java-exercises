d8a17837f7d0e3501cda35cf9a5b941a230578b8
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-10-26 11:08:44 -0500

    created Bob java

---
+++ src/Bob.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] arg){
+        //Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+
+        //Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+        //He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+        //He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+        //He answers 'Whatever.' to anything else.
+        //Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
+
+        //USE SCANNERS LOOPS, ETC
+
+//        boolean confirm;
+//        Scanner grade = new Scanner(System.in);
+//        do {
+//            System.out.print("Enter number grade: ");
+//            int userGrade = grade.nextInt();
+//            if (userGrade < 60) {
+//                System.out.println("F");
+//            } else if (userGrade < 67) {
+//                System.out.println("D");
+//            } else if (userGrade < 80) {
+//                System.out.println("C");
+//            } else if (userGrade < 90) {
+//                System.out.println("B");
+//            } else {
+//                System.out.println("A");
+//            }
+//            System.out.print("Do you want to continue: [Y/N]");
+//            confirm = grade.next().equalsIgnoreCase(("y"));
+//        } while (confirm);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
