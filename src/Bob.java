2bdbc639b3ed8fc428411887dfb871e4491222d7
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-10-26 12:05:49 -0500

    completed ask Bob exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Bob {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         //Bob is a lackadaisical teenager. In conversation, his responses are very limited.
 
         //Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
@@ -9,8 +9,31 @@ public class Bob {
         //He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
         //He answers 'Whatever.' to anything else.
         //Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
-
         //USE SCANNERS LOOPS, ETC
+
+        boolean confirm;
+        Scanner bob = new Scanner(System.in);
+        do {
+            System.out.print("Ask Bob anything or just talk to him: ");
+            String ask = bob.nextLine();
+//        System.out.println("You entered: --> \"" + ask + "\" <--");
+            if (ask.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (ask.endsWith("!")) {
+                System.out.println("Whoa, chill out brah!");
+            } else if (ask.equals("")) {
+                System.out.println("Fine. Be's that guey!");
+            } else {
+                System.out.println("Whatevz");
+            }
+            System.out.print("Do you want to continue to talk Bob: [Y/N]");
+            confirm = bob.next().equalsIgnoreCase(("y"));
+            bob.nextLine();
+        } while(confirm);
+
+        // String userInput2 = scanner.nextLine();
+
+////String austinSentence = "The capital city of Texas is " + txCapitalCity + ", and it is growing quickly.";
 
 //        boolean confirm;
 //        Scanner grade = new Scanner(System.in);
@@ -31,23 +54,6 @@ public class Bob {
 //            System.out.print("Do you want to continue: [Y/N]");
 //            confirm = grade.next().equalsIgnoreCase(("y"));
 //        } while (confirm);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }
