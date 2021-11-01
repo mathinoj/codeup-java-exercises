708cb5ac347e0ea80d5a18a73187d88966cd331a
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-11-01 11:57:19 -0500

    created arrays exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,2 +1,21 @@
 public class ArraysExercises {
+
+    public static void main(String[] arg) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers);
+
+
+        //Array Basics
+        //
+        //Create a class inside of src named ArraysExercises. Create a main method for this class to do your work.
+        //
+        //What happens when you run the following code? Why is Arrays.toString necessary?
+        //
+        //
+        //int[] numbers = {1, 2, 3, 4, 5};
+        //System.out.println(numbers);
+        //Create an array that holds 3 Person objects. Assign a new instance of the Person class to each element. Iterate through the array and print out the name of each person in the array.
+        //
+        //Create a static method named addPerson. It should accept an array of Person objects, as well as a single person object to add to the passed array. It should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array.
+    }
 }
